206945fbc3dbae95387d814b703214e397a8a8a6
ARUN-S01 <a2r0u0n2@gmail.com>
2023-01-05 20:34:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebird/Bullet.java
@@ -0,0 +1,40 @@
+package com.example.firebird;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+import static com.example.firebird.GameView.screenRatioX;
+import static com.example.firebird.GameView.screenRatioY;
+
+import com.example.firebird.R;
+
+
+public class Bullet {
+
+    int x, y, width, height;
+    Bitmap bullet;
+
+    Bullet (Resources res) {
+
+        bullet = BitmapFactory.decodeResource(res, R.drawable.bullet);
+
+        width = bullet.getWidth();
+        height = bullet.getHeight();
+
+        width /= 4;
+        height /= 4;
+
+        width = (int) (width * screenRatioX);
+        height = (int) (height * screenRatioY);
+
+        bullet = Bitmap.createScaledBitmap(bullet, width, height, false);
+
+    }
+
+    Rect getCollisionShape () {
+        return new Rect(x, y, x + width, y + height);
+    }
+
+}
